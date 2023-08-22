7961053b8e36f8ee1c8a11afcaac6aac8d82e75c
DannyMonge1 <142948494+DannyMonge1@users.noreply.github.com>
2023-08-22 17:55:02 -0600

    1
    Creacion de la variable y clase

---
+++ Evaluacion moral.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+class Prices {
+
+	getAveragePrice(ProductsPrices) {
+
+	}
+
+
+};
+
+
+int main()
+{
+	int ProductsPrices[50];
+	for (int i = 0; int i < 50; int i++)
+	{
+		ProductsPrices[] = rand() % 50;
+	};
+	Prices 
+		getAveragePrice(ProductsPrices);
+}
+
